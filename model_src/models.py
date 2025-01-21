b06bc7676e4637542808893539c996a146bdf6e2
zpfinley <33489273+zpfinley@users.noreply.github.com>
2025-01-21 17:42:18 +0800

    Update models.py

--- model_src/models.py
+++ model_src/models.py
@@ -20,6 +20,51 @@ def _get_activation_fn(activation):
     else:
         raise RuntimeError("activation should be relu/gelu, not %s." % activation)
 
+class DecoderLayer(Module):
+    r"""DecoderLayer is mainly made up of the proposed cross-modal relation attention (CMRA).
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model (default=2048).
+        dropout: the dropout value (default=0.1).
+        activation: the activation function of intermediate layer, relu or gelu (default=relu).
+
+    """
+
+    def __init__(self, d_model, nhead, dim_feedforward=1024, dropout=0.1, activation="relu"):
+        super(DecoderLayer, self).__init__()
+
+
+        self.multihead_attn = MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = Linear(d_model, dim_feedforward)
+        self.dropout = Dropout(dropout)
+        self.linear2 = Linear(dim_feedforward, d_model)
+
+        self.norm1 = LayerNorm(d_model)
+        self.norm2 = LayerNorm(d_model)
+        self.dropout1 = Dropout(dropout)
+        self.dropout2 = Dropout(dropout)
+
+        self.activation = _get_activation_fn(activation)
+
+    def forward(self, tgt, memory):
+        r"""Pass the inputs (and mask) through the decoder layer.
+        """
+        memory = torch.cat([memory, tgt], dim=0)
+        tgt2 = self.multihead_attn(tgt, memory, memory)[0]
+        tgt = tgt + self.dropout1(tgt2)
+        tgt = self.norm1(tgt)
+        if hasattr(self, "activation"):
+            tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
+        else:  # for backward compatibility
+            tgt2 = self.linear2(self.dropout(F.relu(self.linear1(tgt))))
+        tgt = tgt + self.dropout2(tgt2)
+        tgt = self.norm2(tgt)
+
+        return tgt
+
 class EncoderLayer(Module):
 
     def __init__(self, d_model, nhead, dim_feedforward=1024, dropout=0.1, activation="relu"):
@@ -51,6 +96,26 @@ class EncoderLayer(Module):
         src = self.norm2(src)
         return src
 
+class Encoder(Module):
+    r"""Encoder is a stack of N encoder layers
+
+    Args:
+        encoder_layer: an instance of the EncoderLayer() class (required).
+        num_layers: the number of sub-encoder-layers in the encoder (required).
+        norm: the layer normalization component (optional).
+
+    """
+
+    def __init__(self, d_model, num_layers, nhead, dim_feedforward):
+        super(Encoder, self).__init__()
+
+        self.encoder_list = nn.ModuleList([EncoderLayer(d_model, nhead, dim_feedforward) for _ in range(num_layers)])
+
+    def forward(self, src):
+
+        for enc in self.encoder_list:
+            src = enc(src)
+        return src
 
 class CMRE_Encoder(Module):
 
